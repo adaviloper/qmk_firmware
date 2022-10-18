91852de90c1d459137867fc4359a2ed6dc06f902
adrian <16654653+adaviloper@users.noreply.github.com>
2022-10-18 14:14:58 -0500

    moved base row definitions to the top

--- users/adaviloper/rows.h
+++ users/adaviloper/rows.h
@@ -4,8 +4,8 @@
 
 #include QMK_KEYBOARD_H
 
-#define LAYOUT_wrapper(...)             LAYOUT(__VA_ARGS__)
-#define LAYOUT_ortho_4x12_wrapper(...)              LAYOUT_ortho_4x12(__VA_ARGS__)
+#define LAYOUT_wrapper(...)                                           LAYOUT(__VA_ARGS__)
+#define LAYOUT_ortho_4x12_wrapper(...)                                LAYOUT_ortho_4x12(__VA_ARGS__)
 
 #define _________________NUMBER_L0_________________                   KC_1, KC_2, KC_3, KC_4, KC_5
 #define _________________NUMBER_R0_________________                   KC_6, KC_7, KC_8, KC_9, KC_0
@@ -17,6 +17,22 @@
 #define _________________QWERTY_R1_________________                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
 #define _________________QWERTY_R2_________________                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN
 #define _________________QWERTY_R3_________________                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
+
+#define _________________MAC_L1____________________                   _________________QWERTY_L1_________________
+#define _________________MAC_L2____________________                   SFT_T(KC_A),  GUI_T(KC_S),  CTL_T(KC_D),  ALT_T(KC_F),  LT(_GIT, KC_G)
+#define _________________MAC_L3____________________                   _________________QWERTY_L3_________________
+
+#define _________________MAC_R1____________________                   _________________QWERTY_R1_________________
+#define _________________MAC_R2____________________                   LT(_GIT, KC_H),  ALT_T(KC_J),  CTL_T(KC_K),  GUI_T(KC_L),  SFT_T(KC_SCLN)
+#define _________________MAC_R3____________________                   _________________QWERTY_R3_________________
+
+#define _________________WINDOWS_L1________________                   _________________MAC_L1____________________
+#define _________________WINDOWS_L2________________                   SFT_T(KC_A),  CTL_T(KC_S),  GUI_T(KC_D),  ALT_T(KC_F),  KC_G
+#define _________________WINDOWS_L3________________                   _________________MAC_L3____________________
+
+#define _________________WINDOWS_R1________________                   _________________MAC_R1____________________
+#define _________________WINDOWS_R2________________                   KC_H,  ALT_T(KC_J),  GUI_T(KC_K),  CTL_T(KC_L),  SFT_T(KC_SCLN)
+#define _________________WINDOWS_R3________________                   _________________MAC_R3____________________
 
 #define _________________FUNC_L0___________________                   _______, _______, _______, _______, _______
 #define _________________FUNC_L1___________________                   _______, VI_WORD, _______, _______, _______
@@ -69,19 +85,3 @@
 #define _________________SYSTEM_R1_________________                   OS_REDO, _______, _______, _______, _______
 #define _________________SYSTEM_R2_________________                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______
 #define _________________SYSTEM_R3_________________                   KC_MOCK, CAP_WRD, CAMEL,   SNAKE,   KEBAB
-
-#define _________________MAC_L1____________________                   _________________QWERTY_L1_________________
-#define _________________MAC_L2____________________                   SFT_T(KC_A),  GUI_T(KC_S),  CTL_T(KC_D),  ALT_T(KC_F),  LT(_GIT, KC_G)
-#define _________________MAC_L3____________________                   _________________QWERTY_L3_________________
-
-#define _________________MAC_R1____________________                   _________________QWERTY_R1_________________
-#define _________________MAC_R2____________________                   LT(_GIT, KC_H),  ALT_T(KC_J),  CTL_T(KC_K),  GUI_T(KC_L),  SFT_T(KC_SCLN)
-#define _________________MAC_R3____________________                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
-
-#define _________________WINDOWS_L1________________                   _________________MAC_L1____________________
-#define _________________WINDOWS_L2________________                   SFT_T(KC_A),  CTL_T(KC_S),  GUI_T(KC_D),  ALT_T(KC_F),  KC_G
-#define _________________WINDOWS_L3________________                   _________________MAC_L3____________________
-
-#define _________________WINDOWS_R1________________                   _________________MAC_R1____________________
-#define _________________WINDOWS_R2________________                   KC_H,  ALT_T(KC_J),  GUI_T(KC_K),  CTL_T(KC_L),  SFT_T(KC_SCLN)
-#define _________________WINDOWS_R3________________                   _________________MAC_R3____________________
